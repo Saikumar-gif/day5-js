a8f87c5ddc842d3955ea26fa0616d937de925e0e
Saikumar-gif <saikumarjadala627@gmail.com>
2025-02-23 13:18:50 +0530

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,36 @@
+//sum of 50 even numbers
+num = 0
+for (i = 1; i<=50; i++){
+    if(i%2==0){
+        num = num+i
+    }
+}
+console.log(num)
+
+
+
+//sum of 20 numbers
+
+num = 0
+for (i = 1; i<=20; i++){
+    num = num+i
+}
+console.log(num)
+
+
+//from 1-10 and 10-1
+//without using < or > operators
+
+
+
+for(var i=1; i!=11; i++)
+
+    {
+        console.log(i)
+    }
+        
+    for(var i=10; i!=0; i--)
+     
+    {
+        console.log(i)
+    }
